5678ef941e41444e99a970f33f96983752eba3f2
Slavik Shen <slavikshen@gmail.com>
2013-02-21 16:11:52 +0800

    Simplify with NSDataDetector
    Replace NSString+FindURL with NSDataDetector.
    Fixi issue that the new string attributes is to be polluted by the old
    attributes remaining.

--- AutoLink/NSTextView+AutomaticLinkDetection.m
+++ AutoLink/NSTextView+AutomaticLinkDetection.m
@@ -7,24 +7,48 @@
 //
 
 #import "NSTextView+AutomaticLinkDetection.h"
-#import "NSString+FindURLs.h"
 #import "NSAttributedString+Hyperlink.h"
 
 @implementation NSTextView (NSTextView_AutomaticLinkDetection)
 
--(void)detectAndAddLinks
-{
-   NSArray *linkLocations = [[self string] locationsOfLinks];
-   NSArray *links = [[self string] arrayOfLinks];
-   
-   int i=0;
-   for( NSString *link in links )
-   {
-      NSAttributedString *linkString = [NSAttributedString hyperlinkFromString:link withURL:[NSURL URLWithString:link]];
-      [[self textStorage] replaceCharactersInRange:[[linkLocations objectAtIndex:i] range] withAttributedString:linkString];
-      i++;
-   }
-   
+//-(void)detectAndAddLinks
+//{
+//   NSArray *linkLocations = [[self string] locationsOfLinks];
+//   NSArray *links = [[self string] arrayOfLinks];
+//   
+//   int i=0;
+//   for( NSString *link in links )
+//   {
+//      NSAttributedString *linkString = [NSAttributedString hyperlinkFromString:link withURL:[NSURL URLWithString:link]];
+//      [[self textStorage] replaceCharactersInRange:[[linkLocations objectAtIndex:i] range] withAttributedString:linkString];
+//      i++;
+//   }
+//   
+//}
+
+- (void)detectAndAddLinks {
+
+    NSTextStorage* textStorage = [self textStorage];
+    NSString* string = [self string];
+    NSUInteger length = [string length];
+    
+    // reset attributes
+    [textStorage setAttributes:nil range:NSMakeRange(0, length)];
+    
+    NSError *error = NULL;
+    NSDataDetector *detector = [NSDataDetector dataDetectorWithTypes:NSTextCheckingTypeLink
+                                                               error:&error];
+    
+    NSArray *matches = [detector matchesInString:string
+                                         options:0
+                                           range:NSMakeRange(0, [string length])];
+    for (NSTextCheckingResult *match in matches) {
+        NSRange range = [match range];
+        NSURL* url = [match URL];
+        NSString* str = [string substringWithRange:range];
+        NSAttributedString *linkString = [NSAttributedString hyperlinkFromString:str withURL:[NSURL URLWithString:link]];
+        [textStorage replaceCharactersInRange:range withAttributedString:linkString];
+    }
 }
 
 @end
